d4ad636028eb334666ad9338759291d37a52e89f
gadsonmulonga <gadsonmulonga@gmail.com>
2024-02-27 18:02:49 +0300

    addition of spinner when logging in

--- lib/Screens/LoginScreen.dart
+++ lib/Screens/LoginScreen.dart
@@ -4,10 +4,10 @@ import 'package:flutter/widgets.dart';
 import 'package:project_manager/Components/InputField.dart';
 import 'package:project_manager/Components/CustomButton.dart';
 import 'package:project_manager/Constants.dart';
-import 'package:project_manager/HomePage.dart';
 import 'package:project_manager/Screens/RegisterScreen.dart';
 import 'package:project_manager/Screens/ForgotPassword.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -20,6 +20,7 @@ class _LoginScreenState extends State<LoginScreen> {
   final _auth = FirebaseAuth.instance;
   String email = '';
   String password = '';
+  bool showSpinner = false;
 
   //Function to validate email format
   bool isValidEmail(String email) {
@@ -32,114 +33,124 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kBgLightColor,
-      body: SafeArea(
-        child: Center(
-          child: ListView(
-            shrinkWrap: true,
-            children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Hero(
-                    tag: 'logo',
-                    child: Image(
-                      image: AssetImage('images/logo.png'),
-                      height: 100,
-                      width: 100,
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: SafeArea(
+          child: Center(
+            child: ListView(
+              shrinkWrap: true,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Hero(
+                      tag: 'logo',
+                      child: Image(
+                        image: AssetImage('images/logo.png'),
+                        height: 100,
+                        width: 100,
+                      ),
                     ),
-                  ),
-                  InputField(
-                    label: 'Email',
-                    onChanged: (value) {
-                      setState(() {
-                        email = value;
-                      });
-                    },
-                  ),
-                  InputField(
-                    label: 'Password',
-                    password: true,
-                    onChanged: (value) {
-                      setState(() {
-                        password = value;
-                      });
-                    },
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                    child: Align(
-                      alignment: Alignment.centerRight,
-                      child: GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => ForgotPassword()),
-                          );
-                        },
-                        child: Text(
-                          'Forgot Password',
-                          style: TextStyle(
-                            color: kBottomAppColor,
-                            decoration: TextDecoration.underline,
+                    InputField(
+                      label: 'Email',
+                      onChanged: (value) {
+                        setState(() {
+                          email = value;
+                        });
+                      },
+                    ),
+                    InputField(
+                      label: 'Password',
+                      password: true,
+                      onChanged: (value) {
+                        setState(() {
+                          password = value;
+                        });
+                      },
+                    ),
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                      child: Align(
+                        alignment: Alignment.centerRight,
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => ForgotPassword()),
+                            );
+                          },
+                          child: Text(
+                            'Forgot Password',
+                            style: TextStyle(
+                              color: kBottomAppColor,
+                              decoration: TextDecoration.underline,
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                  CustomButton(
-                    txtColor: kLightColor,
-                    bgColor: kBottomAppColor,
-                    callBackFunction: () async {
-                      if (isValidEmail(email)) {
-                        final user = await _auth.signInWithEmailAndPassword(
-                            email: email, password: password);
-                        try {
-                          if (user != null) {
-                            Navigator.pushNamed(context, '/homePage');
+                    CustomButton(
+                      txtColor: kLightColor,
+                      bgColor: kBottomAppColor,
+                      callBackFunction: () async {
+                        setState(() {
+                          showSpinner = true;
+                        });
+                        if (isValidEmail(email)) {
+                          final user = await _auth.signInWithEmailAndPassword(
+                              email: email, password: password);
+                          try {
+                            if (user != null) {
+                              Navigator.pushNamed(context, '/homePage');
+                            }
+                            setState(() {
+                              showSpinner = false;
+                            });
+                          } catch (e) {
+                            print(e);
                           }
-                        } catch (e) {
-                          print(e);
+                        } else {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: Text('Error'),
+                                content: Text('Invalid Email'),
+                                actions: <Widget>[
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.of(context)
+                                          .pop(); // Close the dialog
+                                    },
+                                    child: Text('OK'),
+                                  ),
+                                ],
+                              );
+                            },
+                          );
                         }
-                      } else {
-                        showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              title: Text('Error'),
-                              content: Text('Invalid Email'),
-                              actions: <Widget>[
-                                TextButton(
-                                  onPressed: () {
-                                    Navigator.of(context)
-                                        .pop(); // Close the dialog
-                                  },
-                                  child: Text('OK'),
-                                ),
-                              ],
-                            );
-                          },
+                      },
+                      label: 'Login',
+                    ),
+                    CustomButton(
+                      txtColor: kLightColor,
+                      bgColor: kBottomAppColor,
+                      callBackFunction: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => RegisterScreen()),
                         );
-                      }
-                    },
-                    label: 'Login',
-                  ),
-                  CustomButton(
-                    txtColor: kLightColor,
-                    bgColor: kBottomAppColor,
-                    callBackFunction: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => RegisterScreen()),
-                      );
-                    },
-                    label: 'Register',
-                  ),
-                ],
-              ),
-            ],
+                      },
+                      label: 'Register',
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/Screens/RegisterScreen.dart
+++ lib/Screens/RegisterScreen.dart
@@ -5,7 +5,7 @@ import 'package:flutter/widgets.dart';
 import 'package:project_manager/Components/InputField.dart';
 import 'package:project_manager/Components/CustomButton.dart';
 import 'package:project_manager/Constants.dart';
-import 'package:project_manager/HomePage.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({Key? key}) : super(key: key);
@@ -18,6 +18,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
   final _auth = FirebaseAuth.instance;
   String email = '';
   String password = '';
+  bool showSpinner = false;
 
   //Function to validate email format
   bool isValidEmail(String email) {
@@ -30,67 +31,96 @@ class _RegisterScreenState extends State<RegisterScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kBgLightColor,
-      body: SafeArea(
-        child: Center(
-          child: ListView(
-            shrinkWrap: true,
-            children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Hero(
-                    tag: 'logo',
-                    child: Image(
-                      image: AssetImage('images/logo.png'),
-                      height: 100,
-                      width: 100,
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: SafeArea(
+          child: Center(
+            child: ListView(
+              shrinkWrap: true,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Hero(
+                      tag: 'logo',
+                      child: Image(
+                        image: AssetImage('images/logo.png'),
+                        height: 100,
+                        width: 100,
+                      ),
                     ),
-                  ),
-                  InputField(
-                    label: 'Email',
-                    onChanged: (value) {
-                      setState(() {
-                        email = value;
-                      });
-                    },
-                  ),
-                  InputField(
-                    label: 'Password',
-                    password: true,
-                    onChanged: (value) {
-                      setState(() {
-                        password = value;
-                      });
-                    },
-                  ),
-                  CustomButton(
-                    txtColor: kLightColor,
-                    bgColor: kBottomAppColor,
-                    callBackFunction: () async {
-                      if (isValidEmail(email)) {
-                        if (password.length >= 8) {
-                          try {
-                            final newUser =
-                                await _auth.createUserWithEmailAndPassword(
-                              email: email,
-                              password: password,
-                            );
-                            if (newUser != null) {
-                              Navigator.pushNamed(context, '/homePage');
+                    InputField(
+                      label: 'Email',
+                      onChanged: (value) {
+                        setState(() {
+                          email = value;
+                        });
+                      },
+                    ),
+                    InputField(
+                      label: 'Password',
+                      password: true,
+                      onChanged: (value) {
+                        setState(() {
+                          password = value;
+                        });
+                      },
+                    ),
+                    CustomButton(
+                      txtColor: kLightColor,
+                      bgColor: kBottomAppColor,
+                      callBackFunction: () async {
+                        setState(() {
+                          showSpinner = true;
+                        });
+                        if (isValidEmail(email)) {
+                          if (password.length >= 8) {
+                            try {
+                              final newUser =
+                                  await _auth.createUserWithEmailAndPassword(
+                                email: email,
+                                password: password,
+                              );
+                              if (newUser != null) {
+                                Navigator.pushNamed(context, '/homePage');
+                              }
+                              setState(() {
+                                showSpinner = false;
+                              });
+                            } catch (e) {
+                              print(e);
                             }
-                          } catch (e) {
-                            print(e);
+                          } else {
+                            // Show a dialog if the password is less than 8 characters
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: Text('Error'),
+                                  content: Text(
+                                      'Password must be at least 8 characters long'),
+                                  actions: <Widget>[
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.of(context)
+                                            .pop(); // Close the dialog
+                                      },
+                                      child: Text('OK'),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
                           }
                         } else {
-                          // Show a dialog if the password is less than 8 characters
+                          // Show a dialog if the email is invalid
                           showDialog(
                             context: context,
                             builder: (BuildContext context) {
                               return AlertDialog(
                                 title: Text('Error'),
-                                content: Text(
-                                    'Password must be at least 8 characters long'),
+                                content: Text('Invalid Email'),
                                 actions: <Widget>[
                                   TextButton(
                                     onPressed: () {
@@ -104,41 +134,21 @@ class _RegisterScreenState extends State<RegisterScreen> {
                             },
                           );
                         }
-                      } else {
-                        // Show a dialog if the email is invalid
-                        showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              title: Text('Error'),
-                              content: Text('Invalid Email'),
-                              actions: <Widget>[
-                                TextButton(
-                                  onPressed: () {
-                                    Navigator.of(context)
-                                        .pop(); // Close the dialog
-                                  },
-                                  child: Text('OK'),
-                                ),
-                              ],
-                            );
-                          },
-                        );
-                      }
-                    },
-                    label: 'Register',
-                  ),
-                  CustomButton(
-                    txtColor: kLightColor,
-                    bgColor: kBottomAppColor,
-                    callBackFunction: () {
-                      Navigator.pop(context);
-                    },
-                    label: 'Back',
-                  ),
-                ],
-              ),
-            ],
+                      },
+                      label: 'Register',
+                    ),
+                    CustomButton(
+                      txtColor: kLightColor,
+                      bgColor: kBottomAppColor,
+                      callBackFunction: () {
+                        Navigator.pop(context);
+                      },
+                      label: 'Back',
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
