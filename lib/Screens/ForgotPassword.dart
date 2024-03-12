e1838379cef59437a0af45190a957b5856307535
gadsonmulonga <gadsonmulonga@gmail.com>
2024-03-12 12:12:26 +0300

    enabling the user to reset a password

--- lib/Screens/ForgotPassword.dart
+++ lib/Screens/ForgotPassword.dart
@@ -4,8 +4,8 @@ import 'package:flutter/widgets.dart';
 import 'package:project_manager/Components/InputField.dart';
 import 'package:project_manager/Components/CustomButton.dart';
 import 'package:project_manager/Constants.dart';
-
-import 'package:project_manager/Screens/OtpScreen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class ForgotPassword extends StatefulWidget {
   const ForgotPassword({Key? key}) : super(key: key);
@@ -15,52 +15,94 @@ class ForgotPassword extends StatefulWidget {
 }
 
 class _ForgotPasswordState extends State<ForgotPassword> {
+  String email = '';
+  bool showSpinner = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kBgLightColor,
       body: SafeArea(
-        child: Center(
-          child: ListView(
-            shrinkWrap: true,
-            children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Hero(
-                    tag: 'logo',
-                    child: Image(
-                      image: AssetImage('images/logo.png'),
-                      height: 100,
-                      width: 100,
+        child: ModalProgressHUD(
+          inAsyncCall: showSpinner,
+          child: Center(
+            child: ListView(
+              shrinkWrap: true,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Hero(
+                      tag: 'logo',
+                      child: Image(
+                        image: AssetImage('images/logo.png'),
+                        height: 100,
+                        width: 100,
+                      ),
                     ),
-                  ),
-                  InputField(label: 'Email'),
-                  CustomButton(
-                    txtColor: kLightColor,
-                    bgColor: kBottomAppColor,
-                    callBackFunction: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => OtpScreen()),
-                      );
-                    },
-                    label: 'Send OTP',
-                  ),
-                  CustomButton(
-                    txtColor: kLightColor,
-                    bgColor: kBottomAppColor,
-                    callBackFunction: () {
-                      setState(() {
-                        Navigator.pop(context);
-                      });
-                    },
-                    label: 'Back',
-                  ),
-                ],
-              ),
-            ],
+                    InputField(
+                      label: 'Email',
+                      onChanged: (value) {
+                        setState(() {
+                          email = value;
+                        });
+                      },
+                    ),
+                    CustomButton(
+                      txtColor: kLightColor,
+                      bgColor: kBottomAppColor,
+                      callBackFunction: () async {
+                        try {
+                          setState(() {
+                            showSpinner = true;
+                          });
+                          await FirebaseAuth.instance
+                              .sendPasswordResetEmail(email: email);
+                          // Password reset email sent successfully
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: Text('Success'),
+                                content: Text(
+                                    'A reset link has been sent to ${email}'),
+                                actions: <Widget>[
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.of(context)
+                                          .pop(); // Close the dialog
+                                    },
+                                    child: Text('OK'),
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                          setState(() {
+                            showSpinner = false;
+                          });
+                        } catch (e) {
+                          // Handle errors
+                          print(e);
+                        }
+                      },
+                      label: 'Reset Email',
+                    ),
+                    CustomButton(
+                      txtColor: kLightColor,
+                      bgColor: kBottomAppColor,
+                      callBackFunction: () {
+                        setState(() {
+                          Navigator.pop(context);
+                        });
+                      },
+                      label: 'Back',
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
